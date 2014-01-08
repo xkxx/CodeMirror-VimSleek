4e1f8687304a23df9f21ceb5144cf4d09f549b65
xkx <xukexiang@gmail.com>
2014-01-08 13:53:51 -0800

    LightTable plugin support

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject com.lighttable/vim "0.0.1"
-  :description "Vim keybindings for Light Table"
+  :description "Drop-in replacement for vanilla Vim mode, adds vim-seek among other features"
   :dependencies [[org.clojure/clojure "1.5.1"]])
